c43d075b24baa256ee9e7159bd268a210dbb3fa0
ashok <ashokjung@gmail.com>
2019-04-28 13:38:19 +0530

    extended the model

--- src/main/java/com/ashok/springwebapp/model/Author.java
+++ src/main/java/com/ashok/springwebapp/model/Author.java
@@ -3,12 +3,22 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Created by ashok jung bahadur
+ */
+
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
+
+
+    @ManyToMany(mappedBy = "authors")
 
     private Set<Book> books  = new HashSet<>();
 
@@ -56,5 +66,31 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id!=null ? id.equals(author.id):author.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
